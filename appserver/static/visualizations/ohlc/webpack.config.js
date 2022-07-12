90b2e4e7d684d52ed3e7df9e8f0edb231f3583e6
Erica Pescio <edro15@users.noreply.github.com>
2022-07-12 15:10:08 +0200

    Fixes for dependabot alerts on multiple deps

--- appserver/static/visualizations/ohlc/webpack.config.js
+++ appserver/static/visualizations/ohlc/webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
+    mode: "development",
     entry: {
         filename: path.join(__dirname, 'src') + '/visualization_source.js'
     },
